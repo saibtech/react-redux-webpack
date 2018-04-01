7dd125f333267ba5dda58e242cc92db4c7d27ab9
Saikrishna Rengaraj <Saikrishna.Rengaraj@irco.com>
2018-03-31 22:20:55 -0500

    AddedRedux

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,65 @@
-var path = require("path")
+const path = require("path")
+const webpack = require("webpack");
+const CircularDependencyPlugin = require("circular-dependency-plugin");
+
 module.exports = {
     entry: "./src/index.js",
     output : {
         filename : "bundle.js",
-        path: path.resolve(__dirname, "dist")
+        path: path.resolve(__dirname, "src"),
+        publicPath: ""
+    },
+    resolve: {
+        modules: [
+            path.resolve(__dirname, "src"),
+            "node_modules"
+        ],
+        extensions: [".json", ".js", ".jsx"]
     },
     module : {
         rules: [
             {
                 test: /\.jsx?$/,
-                loader: "babel-loader",
+                loader: ["babel-loader"],
                 exclude: /node_modules/
             },
             {
-				test: /\.css$/,
-				use: [
-					"style-loader",
-					{
-						loader: "css-loader",
-						options: {
-							importLoaders: 1,
-							sourceMap: true
-						}
-					},
-					{
-						loader: "postcss-loader",
-						options: {
-							sourceMap: "inline"
-						}
-					}
-				]
-			}
-
+                test: /\.html$/,
+                exclude: /node_modules/,
+                loader: "webpack-transform"
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1,
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            sourceMap: "inline"
+                        }
+                    }
+                ]
+            }
         ]
     },
+    devtool: 'inline-source-map',
     devServer: {
-        contentBase: path.resolve(__dirname, "dist")
-    }
+        contentBase: "./src",
+        hot: true,
+    },
+    plugins: [
+        new CircularDependencyPlugin({
+            exclude: /node_modules/,
+            failOnError: true,
+            cwd: process.cwd()
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
 }
